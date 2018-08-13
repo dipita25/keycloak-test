974e881ff0d7b8e083f040dfa80e8c6ea4f8a042
tokoto dipita alain jonathan <jonathandipita@gmail.com>
2018-08-14 00:40:44 +0200

    initial commit

---
+++ src/main/java/com/example/controllers/keycloakController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author dipita
+ */
+
+@Controller
+public class keycloakController {
+	@GetMapping("/")
+    @ResponseBody
+	public String home() {
+		
+		return "index";
+	}
+}
